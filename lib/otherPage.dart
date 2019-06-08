c9387e1af4f155a79ec3596892925f08bd8a02e6
Mark Malloy <markmalloy96@yahoo.dk>
2019-06-08 21:47:08 +0200

    Programmed functionality into each tab. Now each tab has its own personal padding/widget creation. Each tab is essentially its own page now, that can be prompted from the main tab class.

--- lib/otherPage.dart
+++ lib/otherPage.dart
@@ -34,48 +34,118 @@ class TabBarDemo extends StatelessWidget {
 class OtherScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
+    return new DefaultTabController(
         length: 3,
         child: Scaffold(
           appBar: AppBar(
+            title: Text('Aperture Laboratories'),
             bottom: TabBar(
               tabs: [
                 Tab(icon: Icon(Icons.account_box),text: "Profile",),
                 Tab(icon: Icon(Icons.camera),text: "Science",),
-                Tab(icon: Icon(Icons.accessible), text: "Create new User",),
+                Tab(icon: Icon(Icons.supervised_user_circle), text: "Create new User",),
               ],
             ),
-            title: Text('Aperture Laboratories'),
           ),
 
-          body: TabBarView(
-            children:[ // Each child is the content for each of the 3 tabs.
-            Container(
-              child: Center(
-                child: Text('Tab 1 Layout (Profile)'),
-              ),
-            ),
-
-              Container(
-                child: Center(
-                  child: Text('Tab 2 Layout (Science)'),
-                ),
-              ),
-
-              Container(
-                child: Center(
-                  child: Text('Tab 3 Layout (Create User Form)'),
-                ),
-              )
+          body: new TabBarView(
+            children:<Widget>[ // Each child is the content for each of the 3 tabs.
+              new MenuTab1(),
+              new MenuTab2(),
+              new MenuTab3(),
             ],
+          )),
 
+        );
+  }
+}
+
+class MenuTab1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Tab 1 Layout (Profile)"),
+          new Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: new FlatButton.icon(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              color: Colors.red,
+              textColor: Colors.white,
+              icon: const Icon(Icons.navigate_next, size: 18.0),
+              label: const Text('Log out'),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
 }
+
+class MenuTab2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Tab 2 Layout (Science)"),
+          new Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: new FlatButton.icon(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              color: Colors.red,
+              textColor: Colors.white,
+              icon: const Icon(Icons.navigate_next, size: 18.0),
+              label: const Text('Go To Details'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MenuTab3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Tab 3 Layout (Create User Form)"),
+          new Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: new FlatButton.icon(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              color: Colors.red,
+              textColor: Colors.white,
+              icon: const Icon(Icons.navigate_next, size: 18.0),
+              label: const Text('Go To Details'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
 
 /*
 Below is the original OtherScreen class (Coded by Tobias):
