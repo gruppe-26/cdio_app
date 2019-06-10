8e0fa19f70336adb4fb97f5ea89acd40a924bd5e
keit0017 <kbirongo@gmail.com>
2019-06-10 12:16:57 +0200

    læsning

--- lib/otherPage.dart
+++ lib/otherPage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+//next level
+import 'Menutab1(Profile).dart';
+import 'menutab2(science).dart';
+import 'menutab3(createuser).dart';
 
 class TabBarDemo extends StatelessWidget {
   @override
@@ -60,156 +64,11 @@ class OtherScreen extends StatelessWidget {
   }
 }
 
-class MenuTab1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("Tab 1 Layout (Profile)"),
-          new Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: new FlatButton.icon(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              color: Colors.red,
-              textColor: Colors.white,
-              icon: const Icon(Icons.hotel, size: 18.0),
-              label: const Text('Log out'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
 
-class MenuTab2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("Tab 2 Layout (Science)"),
-          new Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: new FlatButton.icon(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              color: Colors.red,
-              textColor: Colors.white,
-              icon: const Icon(Icons.navigate_next, size: 18.0),
-              label: const Text('Go To Details'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+
+
 //TODO : Add validation to form in MenuTab3. Also clean that shit up
-class MenuTab3 extends StatelessWidget {
-  String _userName;
-  String _firstName;
-  String _initials;
-  int _CPR;
-  String _password;
-  final _formKey = GlobalKey<FormState>();
 
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Scaffold(
-      backgroundColor: Colors.black38,
-      body: SingleChildScrollView(
-        child: new Stack(
-            children: <Widget>[
-          new Column(
-            children: <Widget>[
-            new Form(
-              child: new Theme(
-                data: new ThemeData(
-                    brightness: Brightness.dark, primarySwatch: Colors.blue,
-                    inputDecorationTheme: new InputDecorationTheme(
-                        labelStyle: new TextStyle(color: Colors.white,fontSize: 20.0))),
-                child: Container(
-                  padding: const EdgeInsets.all(40.0),
-                  child: new Column(
-                    crossAxisAlignment: CrossAxisAlignment.center ,
-                    children: <Widget>[
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                          labelText: "Username",
-                          hintText: 'e.g Dragonslayer69',
-                        ),
-                        keyboardType: TextInputType.text,
-                        onSaved: (value) => this._userName = value,
-
-                      ),
-
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                          labelText: "First Name",
-                          hintText: 'e.g Mikkel Mikkelsen',
-                        ),
-                        keyboardType: TextInputType.text,
-                        onSaved: (value) => this._firstName = value,
-                      ),
-
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                          labelText: "Initials",
-                          hintText: '3 letter abbreviation'
-                        ),
-                        keyboardType: TextInputType.text,
-                        onSaved: (value) => this._initials = value,
-                      ),
-
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                          labelText: "Password",
-                          hintText: 'Probably a good idea to keep it a secret ;)',
-                        ),
-                        keyboardType: TextInputType.text,
-                        onSaved: (value) => this._password = value,
-                        obscureText: true,
-                      ),
-
-
-                      new Padding(
-                        padding: const EdgeInsets.only(top:20.0),
-                      ),
-                      new MaterialButton(
-                        color: Colors.blueAccent,
-                        textColor: Colors.white,
-                        child: new Text("Create User"),
-
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/otherPage');
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-
-              ),
-            ],
-        ),
-      ),
-    );
-  }
-}
 
 
 /*
