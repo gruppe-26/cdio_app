f86864bce836de39f7fb9e49ffdef1f5da0be3a2
TristanEmilStenalt <s173839@student.dtu.dk>
2019-06-10 23:49:27 +0200

    User model/class med roles-attribut som er af typen List<String>. +JSON serialisering metoder i klassen - kan også gøres på andre måder, f.eks. ved importering af biblioteker såsom: 'json_serializable' pakken.

--- lib/user.dart
+++ lib/user.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
 
+// Model class
+
 class User {
   String username;
   String name;
   String initials;
   String password;
+  List<String> roles; // printes som: [rolle1, rolle2, rolle3,...,rolleN].
 
-  User(this.username, this.name, this.initials, this.password);
+  User(this.username, this.name, this.initials, this.password, this.roles);
 
+  // Metoder, hvor vi selv står for serialiseringen/konverteringen til JSON-format.
+
+  // .Json constructor, til at konstruere en ny 'User' instans ud fra en Map-struktur.
   User.fromJson(Map<String, dynamic> json)
       : username = json['username'],
         name = json['name'],
         initials = json['initials'],
-        password = json['password'];
+        password = json['password'],
+        roles = json['roles'];
 
+  // toJson() metode, til at konvertere en 'User' instans til en Map-struktur.
   Map<String, dynamic> toJson() => {
     'username': username,
     'name': name,
     'initials': initials,
     'password': password,
+    'roles': roles,
   };
 
+  // toString metode.
+  @override
+  String toString() {
+    return 'User{username: $username, name: $name, initials: $initials, password: $password, roles: $roles}';
+  }
 }
---
+++ lib/userTEMPORARY.dart
@@ -0,0 +1,65 @@
+//import 'package:flutter/material.dart';
+import 'dart:convert'; //importeres for at kunne bruge jsonEncode herinde.
+
+// User  model/class
+class User {
+  // Fields
+  String username;
+  String name;
+  String initials;
+  String password;
+  List<String> roles; // printes som: [rolle1, rolle2, rolle3,...,rolleN].
+
+  // User constructor.
+  User(this.username, this.name, this.initials, this.password, this.roles);
+
+  // Metoder, hvor vi selv står for serialiseringen/konverteringen til JSON-format.
+
+  // .Json constructor, til at konstruere en ny 'User' instans ud fra en Map-struktur.
+  User.fromJson(Map<String, dynamic> json)
+      : username = json['username'],
+        name = json['name'],
+        initials = json['initials'],
+        password = json['password'],
+        roles = json['roles'];
+
+  // toJson() metode, til at konvertere en 'User' instans til en Map-struktur.
+  Map<String, dynamic> toJson() => {
+    'username': username,
+    'name': name,
+    'initials': initials,
+    'password': password,
+    'roles': roles,
+  };
+
+  // toString metode. Autogenereret. Primært til testning.
+  @override
+  String toString() {
+    return 'User{username: $username, name: $name, initials: $initials, password: $password, roles: $roles}';
+  }
+}
+
+// Main til at teste. Brug evt. Dartpad(https://dartpad.dartlang.org/) i stedet.
+void main () {
+  print('Velkommen til: Bruger til JSON-testing \n');
+
+//  List<String> roller = ["Admin", "Pharmaceut"]; //denne er "growable".
+  List<String> roller = new List<String>(); //denne er også "growable".
+  roller.add("Admin");
+  roller.add("Pharmaceut");
+  User bruger = new User("Dragonslayer69", "Tristan", "TES", "Hundkat123", roller);
+
+  print("-----");
+  print("Print af roller: ");
+  print(roller);
+  print("-----");
+  print("Print via bruger toString() -metode:");
+  print(bruger); // det er jo bare toString()-metoden for objektet der kaldes, når man printer et objekt.
+  print("-----");
+  print("Print efter jsonEncode() -metodde:");
+  var userJSON = jsonEncode(bruger); //jsonEncode kigger på objektets selvdefinerede metoder(fromJson, toJson) til konvertering til JSON.
+  print(userJSON);
+  print("-----");
+}
+
+// Note/dokumentation: https://flutter.dev/docs/development/data-and-backend/json#serializing-json-inside-model-classes
