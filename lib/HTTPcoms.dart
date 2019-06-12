7a63017ef40d45ff37b65d053e8644881a83d77b
TristanEmilStenalt <s173839@student.dtu.dk>
2019-06-12 23:03:50 +0200

    Forsøg på kommunikation mellem Flutter og Rest Backend.

--- lib/HTTPcoms.dart
+++ lib/HTTPcoms.dart
@@ -1,20 +1,23 @@
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:cdio_app/user.dart';
 import 'package:http/http.dart' as http;
-import 'dart:io';
-import 'dart:async';
+import 'package:flutter/foundation.dart';
+//import 'dart:io';
 
-String SERVER_URL = "http://192.168.0.29:8080/gruppe26_cdio3_war_exploded/rest/userFlutter";
+String SERVER_URL = "http://192.168.0.18:8080/rest/userFlutter";
 
-Socket socket;
+//Socket socket;
 
+// Add/create user (@POST)
 Future<dynamic> addUserToList(User user) async {
 
   return requestMethod(
       url: SERVER_URL, data: user);
 }
 
+// Helping methods for add/create user.
 requestMethod({String url, User data}) async {
   var body = json.encode(data);
   print("tilføjer ny user: "+body);
@@ -30,10 +33,44 @@ requestMethod({String url, User data}) async {
   print(responseJson);
 
   return response;
+} // end of add/create user
+
+
+//void main() {
+//  //Connect standard in to the socket
+//  stdin.listen((data) => socket.write(new String.fromCharCodes(data).trim() + '\n'));
+//}
+
+
+// Get specific user with id (@GET)
+Future<User> getUser(int id) async {
+  final response = await http.get(SERVER_URL+"/"+id.toString()); //server url + /id
+  if (response.statusCode == 200) {
+    // If server returns an OK(200) response, parse the JSON.
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    // If that response was not OK, throw an error.
+    throw Exception('Failed to load user');
+  }
 }
 
-void main() {
-
-  //Connect standard in to the socket
-  stdin.listen((data) => socket.write(new String.fromCharCodes(data).trim() + '\n'));
+// Get all users (@GET)
+Future<List<User>> getUsersAll() async {
+  final response = await http.get(SERVER_URL);
+  if (response.statusCode == 200) {
+    // If server returns an OK(200) response, parse the JSON using helping method 'parseUsers'.
+    // Using the compute function to run parseUsers in a separate isolate. Helps performance on slower devices.
+    return compute(parseUsers, response.body);
+  } else {
+    // If that response was not OK, throw an error.
+    throw Exception('Failed to load user');
+  }
 }
+
+// A helping function for getUsersAll to convert a response body into a List<User>.
+List<User> parseUsers(String responseBody) {
+  // Caster til en Map<String, dynamic>, da responseBody-JSON'en, egentlig er en Liste af disse User-Maps.
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<User>((json) => User.fromJson(json)).toList();
+}
+
