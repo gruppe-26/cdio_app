78c94c1e464e3708e231f91f632ef9a0b55ca555
Mark Malloy <markmalloy96@yahoo.dk>
2020-03-14 21:08:45 +0100

    it works kinda

--- lib/states/GameTab.dart
+++ lib/states/GameTab.dart
@@ -44,42 +44,62 @@ class _GameTab extends State<Game> with SingleTickerProviderStateMixin {
     return FutureBuilder(
       future: this.getWord(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
-
         switch (snapshot.connectionState){
           case ConnectionState.none:
             //case ConnectionState.waiting:
           // return Center(child: CircularProgressIndicator());
           default:
-            return Column(
-              children: <Widget>[
-                SizedBox(height: 120), // Creates spaces between the different layouts
-                Container(child: Center(child: TextField(
-                    keyboardType: TextInputType.text,
-                    onChanged: (v)=>setState((){_tekst=v;}),
-                    controller: _guess,
-                    ),
-                ))
-                ,
-                SizedBox(height: 100),
-                SizedBox(height: 100),
-                new MaterialButton(
-                  minWidth: 200,
-                  height: 50.0,
-                  color: Colors.blueAccent,
-                  textColor: Colors.white,
-                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                  child: new Text("Gæt",
-                    style: new TextStyle(fontSize: 18),
-                  ),
-                  onPressed: () {
-                      // TODO : Send bogstav til controller
-                    setState(()async{
-                      this._synligtOrd = await guessWord(_guess.text);
-                    });
-                      dispose();
-                  },
-                )
-              ],
+            return Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 120), // Creates spaces between the different layouts
+                    Container(child: Center(child: Text("$_synligtOrd"),
+                    ))
+                    ,
+                    SizedBox(height: 100),
+                    TextFormField(
+                      decoration: new InputDecoration(
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: new BorderSide(
+                          ),
+                        ),
+                        labelText: "Gæt Bogstav",
+                        errorStyle: TextStyle(fontSize: 16.0),
+                      ),
+                      keyboardType: TextInputType.text,
+                      controller: _guess,
+
+                      validator: (value) {
+                        if (value.isEmpty || value.length>1) {
+                          return 'Venligst gæt på ét bogstav';
+                        }
+                        g=value;
+                        return null;
+                      },
+                    )
+                    ,
+                    SizedBox(height: 100),
+                    new MaterialButton(
+                      minWidth: 200,
+                      height: 50.0,
+                      color: Colors.blueAccent,
+                      textColor: Colors.white,
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                      child: new Text("Gæt",
+                        style: new TextStyle(fontSize: 18),
+                      ),
+                      onPressed: () async {
+                        print("trykker på knappen");
+                        // TODO : Send bogstav til controller
+                        this._synligtOrd = await guessWord(_guess.text);
+                        _guess.clear();
+                      },
+                    )
+                  ],
+                ),
+              ),
             );
         }
         });
@@ -88,16 +108,17 @@ class _GameTab extends State<Game> with SingleTickerProviderStateMixin {
   Future<String> getWord() async{
     await Future.delayed(Duration(milliseconds: 400));
     String ord = c.getSynligtOrd();
-    print("ORDET ER!! : "+ord);
+    print("GetWord");
     return ord;
   }
 
 
 
-  guessWord(String guess){
-    return this._memoizer.runOnce(() async {
-      await c.guessWord(guess);
+  guessWord(String guess)async{
+    String synligtOrd;
+      await Future.delayed(Duration(milliseconds: 400));
+      synligtOrd = await c.guessWord(guess);
       print("guessing:::");
-    });
+      return synligtOrd;
   }
 }
